fd4bd5865bb84c0eb5a25c68e24ec6e9a11618df
PenguinCabinet <kage2.kage22@gmail.com>
2024-09-17 10:21:47 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+<div align="center">
+
+# AtCoder-RedirectRecentABC
+
+直近のAtCoder Beginner Contestにリダイレクトするスクリプト
+
+<br>
+<br>
+</div>
+
+## 用途
+直近のAtCoder Beginner Contestに直接アクセスしたい場合に使ってください。
+
+## 🔨 使い方
+このスクリプトを入れた状態で、    
+[https://atcoder.jp/contests#recent_abc](https://atcoder.jp/contests#recent_abc)    
+にアクセスすると、直近のAtCoder Beginner Contestにリダイレクトされます。    
+### 直近のAtCoder Beginner Contestとは
+1. 開催中のAtCoder Beginner Contestがある場合、そこにリダイレクトします
+2. 「開催中のコンテスト」がない場合、「予定されたコンテスト」の中で、最小の番号のAtCoder Beginner Contestにリダイレクトします
+3. 1.2.両方を満たさない場合、リダイレクトしません。
+
+### Diffオプション
+[https://atcoder.jp/contests#recent_abc-1](https://atcoder.jp/contests#recent_abc-1)のように後ろに数値を入れると、直近の番号からその数を引いたAtCoder Beginner Contestに飛びます。    
+土曜日に行ったABCを復習したい場合にどうぞ。
+
+> [!WARNING]
+> このスクリプトはMIT LICENSEで提供されています。バグなどによって、何らかの不利益があった場合でも、作者は一切責任を負いません。
+
+## ⬇️ インストール
+[greasyfork]()のページからインストールしてください。    
+Tampermonkeyなどのユーザースクリプト実行アドオンが別途必要です。    
+
+
+## 🎫 LICENSE
+
+[MIT](./LICENSE)
+
+## ✍ Author
+
+[PenguinCabinet](https://github.com/PenguinCabinet)
---
+++ main.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         AtCoder-RedirectRecentABC
+// @namespace    https://github.com/PenguinCabinet
+// @version      v0.0.1
+// @description  The tools to redirect the recent AtCoder Beginner Contest.
+// @author       PenguinCabinet
+// @license      MIT
+// @match        https://atcoder.jp/contests*
+// @grant        none
+// ==/UserScript==
+
+//config
+//config
+
+async function Newest_N(elem_id1, elem_id2) {
+    const r = /AtCoder Beginner Contest ([1-9][0-9]*)/g;
+
+    const contests_HTML_text = await (await fetch("https://atcoder.jp/contests/")).text();
+    const doc = new DOMParser().parseFromString(contests_HTML_text, "text/html");
+    let elem = doc.getElementById(elem_id1);
+    if (!elem) {
+        elem = doc.getElementById(elem_id2);
+    }
+    const text = elem.outerHTML;
+    const ABC_texts = [...text.matchAll(r)];
+    const Numbers = ABC_texts.map((e) => parseInt(e[1]));
+
+    return Math.min.apply(null, Numbers);
+}
+
+(async function () {
+    'use strict';
+
+    let urlHash = location.hash;
+    if (urlHash) {
+        const r = /#recent_abc((\-|\+)[1-9][0-9]*)?/;
+        const result = urlHash.match(r);
+        if (result != null) {
+            let Ans = await Newest_N("contest-table-action", "contest-table-upcoming");
+
+            let diff = result[1];
+            if (diff === undefined) diff = 0;
+            diff = parseInt(diff, 10);
+
+            if (Ans != null) {
+                window.location.replace(`https://atcoder.jp/contests/abc${Ans + diff}`);
+            }
+        }
+    }
+    // Your code here...
+})();
